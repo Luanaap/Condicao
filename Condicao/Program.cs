065ef965901ce082be266abcf37218bc5f84239a
Luanaap <57630311+Luanaap@users.noreply.github.com>
2023-02-24 23:13:03 -0300

    Criando repositorio para Condicao

---
+++ Condicao/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Condição encadeada
+            //int valor = 15;
+
+            //if(valor < 5)
+            //{
+            //    Console.WriteLine("Condição verdadeira");
+            //}
+            //else if ( valor >= 5 && valor < 10)
+            //{
+            //    Console.WriteLine("Condição alternativa 1");
+            //}
+            //else if(valor >= 10 && valor < 20)
+            //{
+            //    Console.WriteLine("Condição alternativa 2");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Condição alternativa final");
+            //}
+            #endregion
+
+            #region Condição aninhada
+
+            //int numero = 13;
+
+            //if(numero > 5)
+            //{
+            //    Console.Write("O número é maior que 5");
+
+            //    if(numero % 2 == 0)
+            //    {
+            //        Console.WriteLine(" e também é par");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(" mas não é par.");
+            //    }
+
+            //}
+
+            #endregion
+
+            int numero = 3;
+            //string mensagem = numero > 5 ? "Maior que 5" : "Menor que 5";
+
+            // condição ? true : false;
+            //mensagem = numero > 5 ? "Maior que 5" : "Menor que 5";
+
+
+            //if(numero > 5)
+            //{
+            //    mensagem = "maior que 5";
+            //}
+            //else
+            //{
+            //    mensagem = "Menor que 5";
+            //}
+
+            Console.WriteLine(numero > 5 ? "Maior que 5" : "Menor que 5");
+            Console.ReadKey();
+        }
+    }
+}
